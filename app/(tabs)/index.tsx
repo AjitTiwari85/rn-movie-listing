e88f434d95b5f218ad1c4b60718930a7834ce7a2
Ajit Tiwari <at856575@gmail.com>
2026-01-20 15:34:23 +0530

    add tab navigation and screens for home, profile, saved, search, and movie details

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,13 @@
+import { Text, View } from "react-native";
+
+function Index() {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-5xl">Welcome!</Text>
+  
+    </View>
+  );
+}
+
+export default Index;
+
