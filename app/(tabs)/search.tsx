d021eac12354009a5866834c034a47ea2d9b131d
Ajit Tiwari <at856575@gmail.com>
2026-01-28 03:28:33 +0530

    refactor search component to implement movie fetching and display using FlatList

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,14 +1,43 @@
+import MovieCard from '@/components/MovieCard'
+import { images } from '@/constants/images'
+import { fetchMovies } from '@/services/api'
+import useFetch from '@/services/usefetch'
+import { useRouter } from 'expo-router'
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { FlatList, Image, View } from 'react-native'
 
 const search = () => {
+
+  const router = useRouter();
+  const {data: movies,
+     loading: moviesLoading,
+      error: movieError} = useFetch(() => fetchMovies( {
+    query: ''
+
+  }))
+
+
   return (
-    <View>
-      <Text>search</Text>
+    <View className='flex-1 bg-primary'>
+      <Image source={images.bg} className=" flex-1 absolute w-full z-0"
+      resizeMode='cover'/>
+      <FlatList data={movies}
+          renderItem={({item}) => <MovieCard {... item}/>}
+          keyExtractor={(item) => item.id.toString()}
+          className='px-5'
+          numColumns={3}
+          columnWrapperStyle={{
+            justifyContent: 'center',
+            gap: 16,
+            marginVertical: 16
+          }}
+
+          contentContainerStyle={{
+            paddingBottom: 100,
+          }}
+           />
     </View>
   )
 }
 
 export default search
-
-const styles = StyleSheet.create({})
